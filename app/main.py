e8e4bd0ee336bbbd8dae2891cc33e4091c68f1a0
jessica.greene <jessica.greene@ecosia.org>
2024-05-14 22:56:29 -0400

    reset start branch

--- app/main.py
+++ app/main.py
@@ -2,10 +2,8 @@ import requests
 import random
 import time
 
-from codecarbon import EmissionsTracker
 from os import getenv
-from http.server import HTTPServer
-from prometheus_client import MetricsHandler, Counter, Histogram
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from string import Template
 from util import artificial_503, artificial_latency
 
@@ -21,21 +19,10 @@ ELECTRICITY_MAP_API_KEY = getenv("ELECTRICITY_MAP_API_KEY")
 
 # GIVEN ZONE - FEEL FREE TO CHANGE
 ZONE = "DE"
-carbon_intensity_url = f"https://api.electricitymap.org/v3/carbon-intensity/latest?zone={ZONE}"
-# Prometheus counter to count number of requests by status and endpoint
-requestCounter = Counter("requests_total", "total number of requests", ["status", "endpoint"])
-
-# Prometheus histogram to track latency of requests
-requestHistogram = Histogram("request_latency_seconds", "Request latency", ["endpoint"])
-requestHistogramCarbonIntensity = requestHistogram.labels(endpoint="/carbon_intensity")
-
-
-# Tracker for carbon emissions
-tracker = EmissionsTracker(
-    project_name="python-app",
-    save_to_prometheus=True,
-    prometheus_url="http://pushgateway:9091",
+carbon_intensity_url = (
+    f"https://api.electricitymap.org/v3/carbon-intensity/latest?zone={ZONE}"
 )
+
 
 with open("./templates/carbonIntensity.html", "r") as f:
     html_string = f.read()
@@ -43,21 +30,17 @@ html_template = Template(html_string)
 
 
 def fetch_carbon_intensity():
-    tracker.start()
     r = (
         requests.get(carbon_intensity_url, auth=("auth-token", ELECTRICITY_MAP_API_KEY))
         if random.random() > 0.15
         else artificial_503()
     )
-    requestCounter.labels(status=r.status_code, endpoint="/upstream").inc()
-    _ = tracker.stop()
     if r.status_code == 200:
         return r.json()["carbonIntensity"]
     return 0
 
 
-class HTTPRequestHandler(MetricsHandler):
-    @requestHistogramCarbonIntensity.time()
+class HTTPRequestHandler(BaseHTTPRequestHandler):
     @artificial_latency
     def get_carbon_intensity(self):
         self.do_HEAD()
@@ -65,7 +48,6 @@ class HTTPRequestHandler(MetricsHandler):
         bytes_template = bytes(
             html_template.substitute(counter=carbon_intensity, zone=ZONE), "utf-8"
         )
-        requestCounter.labels(status="200", endpoint="/carbon_intensity").inc()
         self.wfile.write(bytes_template)
 
     def do_HEAD(self):
